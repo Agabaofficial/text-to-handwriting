6540d75c72c96141c5c9a7c9aa35d31679c42db4
Agaba Olivier <125471253+Agabaofficial@users.noreply.github.com>
2023-12-28 02:26:08 +0200

    Update app.mjs

--- js/app.mjs
+++ js/app.mjs
@@ -1,16 +1,14 @@
-<script type="module">
 import {
   addFontFromFile,
   formatText,
   addPaperFromFile
-} from 'https://raw.githubusercontent.com/Agabaofficial/text-to-handwriting/master/js/utils/helpers.mjs
-';
+} from './utils/helpers.mjs';
 import {
   generateImages,
   downloadAsPDF,
   deleteAll
 } from './generate-images.mjs';
-import { setInkColor, toggleDrawCanvas } from 'https://raw.githubusercontent.com/Agabaofficial/text-to-handwriting/master/js/utils/draw.mjs';
+import { setInkColor, toggleDrawCanvas } from './utils/draw.mjs';
 
 /**
  *
@@ -207,4 +205,4 @@ fetch(
       )
       .join('');
   });
-</script>
+
